6a0c249e9d13dc9645f130acea2a3aba2b6b9308
Egon Axelsson <egonaxelsson@hotmail.se>
2020-11-16 19:51:36 +0100

    Pushar för Stationär ska få in.

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,14 +1,14 @@
 
 var food = [
   {
-  name: 'Basic Burger',
-  price: 130,
-  kCal: 410,
-  lactose: true,
-  gluten: true,
-  img: "https://hankstruebbq.se/wp-content/uploads/2019/06/Smash_burgers.jpg"
-  },
-
+  "name": 'Basic Burger',
+  "price": 130,
+  "kCal": 410,
+  "lactose": true,
+  "gluten": true,
+  "img": "https://hankstruebbq.se/wp-content/uploads/2019/06/Smash_burgers.jpg"
+  }
+,
 {
 "name": 'Veggo Burger',
 "price": 130,
@@ -16,7 +16,8 @@ var food = [
 "lactose": false,
 "gluten": false,
 "img": "https://static01.nyt.com/images/2020/01/22/dining/jo-black-bean-burgers/merlin_167531589_227b9414-ffad-4b44-ae53-67483bd2bae5-articleLarge.jpg"
-},
+}
+,
 {
 "name": 'BBQ Burger',
 "price": 160,
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -3,10 +3,17 @@
     el: '#Burger_menu_div',
     data: {
       burgers: [
-        new MenuItem(food(1)),
-        new MenuItem(food(2)),
-        new MenuItem(food(3)),
- ],
-
+        new MenuItem(food[0]['name'], food[0]['price'], food[0]['kCal'], food[0]['lactose'], food[0]['gluten'], food[0]['img']),
+        new MenuItem(food[1]['name'], food[1]['price'], food[1]['kCal'], food[1]['lactose'], food[1]['gluten'], food[1]['img']),
+        new MenuItem(food[2]['name'], food[2]['price'], food[2]['kCal'], food[2]['lactose'], food[2]['gluten'], food[2]['img']),
+]
 }
 })
+new Vue({
+    el: '#OrderButton',
+    methods: {
+        markDone: function() {
+              console.log('Kliuckad');
+        }
+    }
+});
